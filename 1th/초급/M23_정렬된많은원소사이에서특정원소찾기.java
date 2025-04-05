4841a35ef2a561afc47b698255dffc5664c1be7a
dahxxn <dahyeonhan4529@gmail.com>
2025-04-05 16:40:48 +0900

    feat(java): [초급] M23_정렬된많은원소사이에서특정원소찾기

--- 1th/초급/M23_정렬된많은원소사이에서특정원소찾기.java
+++ 1th/초급/M23_정렬된많은원소사이에서특정원소찾기.java
@@ -1,4 +1,26 @@
+import java.io.*;
+import java.util.*; 
+
+//8 : 22 - 8 : 26
 
 public class M23_정렬된많은원소사이에서특정원소찾기 {
+    public static void main(String[] args) throws IOException { 
 
+        Scanner scanner = new Scanner(System.in); 
+
+        int N = scanner.nextInt(); 
+
+        int A = scanner.nextInt(); 
+        
+        for(int i = 0 ; i<N ; i++){
+            int currentNum = scanner.nextInt(); 
+            if(currentNum == A){
+                System.out.println(i+1); 
+                break; 
+            }else if(currentNum > A){
+                System.out.println(-1); 
+                break; 
+            }
+        }
+    }
 }
